9cba6e4895257614bcbb8659e8262c52d73480d5
yytlee <yytlee2712@gmail.com>
2020-01-28 13:56:03 +0800

    add and remove files

--- assignment1/skeleton/halftone.cpp
+++ assignment1/skeleton/halftone.cpp
@@ -10,11 +10,13 @@ halftone.cpp
 */
 
 #include "bmp.h"		//	Simple .bmp library
+#include <iostream>
 
 #define MAX_SHADES 3
 
 #define SAFE_FREE(p)  { if(p){ free(p);  (p)=NULL;} }
 
+using namespace std;
 
 int main(int argc, char** argv)
 {
@@ -24,6 +26,11 @@ int main(int argc, char** argv)
 	//	1. Open BMP file 
 	//  you can use the following parameters for testing,
 	//  remember to modify it based on the user input.
+	
+	if(argc != 4){
+		cout << "Error: input format should be [halftone <input.bmp> <size1> <size2>]" << endl;
+	}
+
 	int imagesize = 64;
 	int patchsize = 16;
 	Bitmap image_data("monokuma.bmp"); 
