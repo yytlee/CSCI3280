c2d1724203dadd19bde127f2c0e4f390b61713bc
yytlee <yytlee2712@gmail.com>
2020-06-11 20:56:39 +0800

    add readme and fix

---
+++ assignment4/skeleton/Readme.txt
@@ -0,0 +1,3 @@
+I did the basic and the enhanced part.
+
+Compile the code with cl dct.cp bmp.cpp
--- assignment4/skeleton/dct.cpp
+++ assignment4/skeleton/dct.cpp
@@ -9,6 +9,9 @@ using namespace std;
 #define PI 3.14159265358979
 int QuantizationMatrix[64] = {3, 5,	7, 9, 11, 13, 15, 17, 5, 7,	9, 11, 13, 15, 17, 19, 7, 9, 11, 13, 15, 17, 19, 21, 9, 11,	13,	15,	17,	19,	21,	23, 11,	13,	15,	17,	19,	21,	23,	25, 13,	15,	17,	19,	21,	23,	25,	27, 15,	17,	19,	21,	23,	25,	27,	29, 17,	19,	21,	23,	25,	27,	29,	31};
 
+double c(int x){
+	return x == 0? (1 / sqrt(2)) : 1;
+}
 
 int main(int argc, char** argv)
 {
@@ -27,6 +30,18 @@ int main(int argc, char** argv)
 	
 	//! preprocess by shifting pixel values by 128
 	//TODO
+
+	// double s_arr[256][256];
+	// for(int i = 0; i < rows; i++){
+	// 	for(int j = 0; j < cols; j++){
+	// 		unsigned char tmp = '\0';
+	// 		s_img.getPixel(i, j, tmp);
+	// 		//cout << (int)tmp << " ";
+	// 		// tmp = (int)tmp - 128;
+	// 		s_arr[i][j] = (double)tmp;
+	// 	}
+	// 	//cout << endl;
+	// }
 	
 	//! 2D DCT for every 8x8 block (assume that the input image resolution is fixed to 256)	
 	double coeffArray[256][256]={0};
@@ -38,9 +53,42 @@ int main(int argc, char** argv)
 			int xpos = j*8, ypos = i*8;
 			//! apply DCT on block_ij (basic requirement)
 			//TODO
+			long double rp[8][8] = {0};
+			for(int v = 0; v < 8; v++){
+				for(int u = 0; u < 8; u++){
+					long double sum = 0;
+					for(int x = 0; x < 8; x++){
+						unsigned char pix = 0;
+						s_img.getPixel(xpos + x, ypos + v, pix);
+						// double pix = s_arr[xpos + x][ypos + v];
+						long double pix_d  = pix - 128;
+						sum += (long double)pix_d * cos(u * PI * (2 * x + 1) / 16);
+					}
+					rp[u][v] = c(u) / 2 * sum;
+				}
+			}
+
+			for(int u = 0; u < 8; u++){
+				for(int v = 0; v < 8; v++){
+					long double sum = 0;
+					for(int y = 0; y < 8; y++){
+						long double pix = rp[u][y];
+						sum += pix * cos(v * PI * (2 * y + 1) / 16);
+					}
+					coeffArray[xpos + u][ypos + v] = c(v) / 2 * sum;
+				}
+			}
 
 			//! quantize the frequency coefficient of this block (enhanced part)
 			//TODO
+			if(need_quantization){
+				for(int m = 0; m < 8; m++){
+					for(int n = 0; n < 8; n++){
+						int ind = m * 8 + n;
+						coeffArray[xpos + m][ypos + n] = round(coeffArray[xpos + m][ypos + n] / QuantizationMatrix[ind]);
+					}
+				}
+			}
 			
 		}
 	}
